cfb5aea946065db982fdce41966789607ca3533b
Mark Rickerby <me@maetl.net>
2011-06-27 12:12:01 +1000

    adding skeletal cli runner

--- Faker.php
+++ Faker.php
@@ -139,4 +139,29 @@ class Faker {
 		return $string;
 	}
 
+	public function __get($property) {
+		$classname = 'Fake_' . $property;
+		return new $classname;
+	}
+	
+	/**
+ 	 * Command line runner. Generates fake data from method specified by arguments.
+	 *
+	 * @param array $arguments
+	 */
+	static public function main($arguments) {
+		
+		if (!isset($arguments[1]) || $arguments[1] == '--help' || $arguments[1] == '-h') {
+			echo "Faker\nA fake data generator.\n\nUsage:\n$ faker [method]\n$ faker [type]:[method]\n\n";
+			return;
+		}
+
+		$targets = explode(':', $arguments[1]);
+		$method = array_pop($targets);
+		$object = 'Fake_' . implode('_', array_map('ucfirst', $targets));
+
+		$faker = new $object();
+		echo $object->method();
+		return;		
+	}
 }
