37c5317978f8f4bf8ec016bf0464971d72cdea1b
maetl <me@maetl.net>
2011-07-06 23:33:26 +1000

    adding RGB colors

--- Fake/Color.php
+++ Fake/Color.php
@@ -1,9 +1,23 @@
 <?php
+/**
+ * Faker {{version}}
+ * A Fake Text Generator
+ * <http://github.com/maetl/Faker>
+ *
+ * Copyright (c) 2011, Mark Rickerby <http://maetl.net>
+ * All rights reserved.
+ * 
+ * This library is free software; refer to the terms in the LICENSE file found
+ * with this source code for details about modification and redistribution.
+ */
 
+/**
+ * A randomized color generator.
+ */
 class Fake_Color extends Fake {
 	
 	/**
-	 * Return an HTML-style hex value representing a random color.
+	 * Return a random HTML-style hex color.
 	 */
 	static public function hex() {
 		$color = "#";
@@ -11,6 +25,14 @@ class Fake_Color extends Fake {
     		$color .=  dechex(rand(0,15));
     	}
     	return $color;
-	}	
+	}
+	
+	/**
+	 * Return a random RGB color with 8-bits per channel.
+	 */
+	static public function rgb($glue=',') {
+		$triplet = implode($glue, array(rand(0,255), rand(0,255), rand(0,255)));
+		return "($triplet)";
+	}
 	
 }
