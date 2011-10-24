e2321726b87078606773d2923428ac6f488e60d0
maetl <me@maetl.net>
2011-10-24 22:29:15 +1100

    register english language (not yet used)

--- Faker/Locale.php
+++ Faker/Locale.php
@@ -1,7 +1,31 @@
 <?php
+/**
+ * Faker {{version}}
+ * A Fake Text Generator
+ * <http://github.com/maetl/Faker>
+ *
+ * Copyright (c) 2011, Mark Rickerby <http://maetl.net>
+ * All rights reserved.
+ * 
+ * This library is free software; refer to the terms in the LICENSE file found
+ * with this source code for details about modification and redistribution.
+ */
 
+/**
+ * @todo support variations of locales
+ */
 class Faker_Locale {
 	
+	/**
+	 * Registered languages.
+	 */
+	private static $languages = array(
+		"en" => "English"
+	);
+	
+	/**
+	 * Current locale.
+	 */
 	private static $locale;
 	
 	/**
